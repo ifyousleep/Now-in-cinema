c6471cfbec9be9f6feaa32cc1751eee177afda3c
ifyousleep <ifyousleep@gmail.com>
2017-04-06 20:57:21 +0300

    StateStrategyType for error

--- app/src/main/java/com/ifyou/nowincinema/presentation/view/MovieListView.java
+++ app/src/main/java/com/ifyou/nowincinema/presentation/view/MovieListView.java
@@ -1,12 +1,15 @@
 package com.ifyou.nowincinema.presentation.view;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.ifyou.nowincinema.model.ResultsItem;
 
 import java.util.List;
 
 public interface MovieListView extends MvpView {
 
+    @StateStrategyType(SkipStrategy.class)
     void showError(String error);
 
     void hideProgressBar();
